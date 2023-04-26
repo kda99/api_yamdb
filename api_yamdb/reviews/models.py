95b9a0a87b65eb88c36efb85d76c2a4422d898b3
Ìêàêñè Ìàòâååâ <Filosf5@yandex.ru>
2023-04-26 13:12:40 +0300

    Update setting, Update Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -31,24 +31,12 @@ class Title(models.Model):
     #возможно, это не правильно. Это правильно при условии, что можно выбрать несколько жанров
     #для одного произведения. Но, как я понял, можно выбрать только один жанр. Буду разбираться.
     genre = ArrayField(
-            models.CharField(max_length=10, blank=True),
-        )
+        models.CharField(max_length=10, blank=True),
+    )
     category = models.ForeignKey(Category, verbose_name='Категории')
 
     def __str__(self):
         return self.name
-
-
-class Comment(models.Model):
-    """Модель комментария к отзыву"""
-    #title_id integer (ID произведения)
-    #review_id integer (ID отзыва)
-    #"id": 0, 
-    #"text": "string", (Текст комментария)
-    #"author": "string",
-    #"pub_date": "2019-08-24T14:15:22Z"
-
-    pass
 
 
 class Review(models.Model):
@@ -80,3 +68,44 @@ class Review(models.Model):
                 fields=['author', 'titel'], name='unique_reviewing'
             )
         ]
+
+
+class Comment(models.Model):
+    """Модель комментария к отзыву"""
+    #title_id integer (ID произведения)
+    #review_id integer (ID отзыва)
+    #"id": 0, 
+    #"text": "string", (Текст комментария)
+    #"author": "string",
+    #"pub_date": "2019-08-24T14:15:22Z"
+    author = models.ForeignKey(
+        #User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария'
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв'
+    )
+    titel = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Произведение'
+    )
+    text = models.TextField(verbose_name='Комментарий')
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Коментарии'
+
+    def __str__(self):
+        return '"{}" to review "{}" by author "{}"'.format(
+            self.text, self.review, self.author
+        )
