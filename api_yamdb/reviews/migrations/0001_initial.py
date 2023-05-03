8f868fed67fba2fea3a293cd3b12cac8cf4e7bd2
Дмитрий Капустин <kda99@mail.ru>
2023-05-03 22:05:02 +0300

    10/67

--- api_yamdb/reviews/migrations/0001_initial.py
+++ api_yamdb/reviews/migrations/0001_initial.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
+                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last_login')),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
